68d2785336365f7dd287f30c2975cf51dfcf952d
GertenP <gerten.pilv@tptlive.ee>
2024-11-09 14:58:17 +0200

    add Sonum.cs

---
+++ MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project1
+{
+    internal class MainClass
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the Sõnum class
+            Sõnum tekst = new Sõnum();
+
+            // Assigning values to the properties
+            tekst.Sisu = "Väga suur sisu";
+            tekst.Autor = "Gerten";
+            tekst.Aeg = 50;
+            tekst.MeeldimisteArv = 99;
+
+            // Calling the meeldivus method
+            tekst.Meeldivus();
+        }
+    }
+}
---
+++ Sõnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Sõnum
+    {
+        public String Sisu { get; set; }
+        public String Autor { get; set; }
+        public int Aeg { get; set; }
+        public int MeeldimisteArv { get; set; }
+
+        public void Meeldivus()
+        {
+            int meeldivus = MeeldimisteArv / Aeg;
+            Console.WriteLine($"Meeldivus sellel sõnumil on {meeldivus}");
+        }
+
+    }
+}
